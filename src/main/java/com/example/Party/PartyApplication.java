eb095c48fab92c331f8b037ad9f2eeb18dce4fd3
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-06-27 10:43:55 +0200

    1

---
+++ src/main/java/com/example/Party/PartyApplication.java
@@ -0,0 +1,15 @@
+package com.example.Party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/example/Party/PartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
