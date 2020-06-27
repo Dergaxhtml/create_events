6d35da2e5c5d6628b6a6a5b64804aa3fb550df86
Norbert <syskaxiii@gmail.com>
2020-06-27 12:43:59 +0200

    model user created

--- src/main/java/com/example/Party/PartyApplication.java
+++ src/main/java/com/example/Party/PartyApplication.java
@@ -2,6 +2,8 @@ package com.example.Party;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PartyApplication {
@@ -9,5 +11,7 @@ public class PartyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PartyApplication.class, args);
 	}
-
 }
+
+
+
