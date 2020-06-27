c9cc91d9a72b5044b64ade8afe1420235d51c044
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-06-27 12:40:19 +0200

    created controller and repository dependency

--- src/main/java/com/example/Party/controller/PortalUserController.java
+++ src/main/java/com/example/Party/controller/PortalUserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PortalUserController {
     @Autowired
-    PortalUserRepository portalUserRepository;
+    private PortalUserRepository portalUserRepository;
 
     @GetMapping("/home")
     public String homePage(){
