9d16d716838d5c54d1bdd12b47d132a99f39065d
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-06-27 13:24:05 +0200

    rename dirctory and import repository

--- src/main/java/com/sda/party/repository/PortalUserRepository.java
+++ src/main/java/com/sda/party/repository/PortalUserRepository.java
@@ -1,5 +1,6 @@
-package com.example.Party.repository;
+package com.sda.party.repository;
 
+import com.sda.party.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
