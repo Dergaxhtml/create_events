24b1fdd901dbeddacf342646e31fdf048b117710
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-07-04 15:13:50 +0200

    name controller to RegisterController

--- src/main/java/com/sda/party/controller/RegisterController.java
+++ src/main/java/com/sda/party/controller/RegisterController.java
@@ -14,7 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class PortalUserController {
+public class RegisterController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
