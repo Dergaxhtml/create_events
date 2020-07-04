7316a9f61405f631153bb360ee98ef7d730b58f3
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-07-04 15:32:52 +0200

    repairv2

--- src/main/java/com/sda/party/controller/EventController.java
+++ src/main/java/com/sda/party/controller/EventController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public class EventController {
+
     @Autowired
     private EventRepository eventRepository;
 
